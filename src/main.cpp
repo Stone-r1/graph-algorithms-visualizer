ad5b639b3d0bed8f4cf7cca5e628924ef5f2e3f1
stoney <sarkisyanmihail11@gmail.com>
2025-07-21 00:33:55 +0400

    feat(main): basic setup. user can draw nodes and edges

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
+#include "board.h"
 #include <iostream>
+#include "raylib.h"
 
 int main() {
-    std::cout << "HELLO WORLD\n";
+    int monitor = GetCurrentMonitor();
+    InitWindow(1200, 800, "Graph Visualizer");
+    SetWindowMinSize(1200, 800);
+    SetTargetFPS(60);
+
+    Board board;
+    Vector2 selectedNode = {-1, -1};
+
+    while (!WindowShouldClose()) { 
+        Vector2 mouse = GetMousePosition();
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            board.addNode(mouse);
+        }
+
+        if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            Vector2 nodePosition = board.isInNodeDomain(mouse);
+            if (nodePosition.x != 0.0f && nodePosition.y != 0.0f) {
+                if (selectedNode.x == -1) {
+                    selectedNode.x = nodePosition.x;
+                    selectedNode.y = nodePosition.y;
+                    std::cout << "Node 1 was chosen successfully\n";
+                } else {
+                    std::cout << "Added the edge between Node 1 and Node2\n";
+                    board.addEdge(selectedNode, nodePosition);
+                    selectedNode = {-1, -1};
+                }
+            }
+        }
+
+        BeginDrawing();
+            ClearBackground(WHITE);
+            board.drawNodes();
+            board.drawEdges();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
