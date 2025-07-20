46d60bad77e4e95e4c90acc7bb11a6e023dae45c
stoney <sarkisyanmihail11@gmail.com>
2025-07-20 20:06:22 +0400

    feat(node): added default constructor, neighbor tracker n some additional functions

--- src/node.cpp
+++ src/node.cpp
@@ -1,12 +1,20 @@
 #include "raylib.h"
 #include "node.h"
 
-Node::Node(int index, Vector2 position, float radius) {
-    nodeIndex = index;
-    nodePosition = position;
-    nodeRadius = radius;
-    isActive = false;
-}
+
+Node::Node(int index, Vector2 position, float radius) :
+    nodeIndex(index),
+    nodePosition(position),
+    nodeRadius(radius),
+    isActive(false)
+{}
+
+Node::Node() : 
+    nodeIndex(-1),
+    nodePosition({0, 0}),
+    nodeRadius(0.0f),
+    isActive(false)
+{}
 
 int Node::getNodeIndex() const {
     return nodeIndex;
@@ -24,10 +32,26 @@ bool Node::isNodeActive() const {
     return isActive;
 }
 
+bool Node::isNodeValid() const {
+    return !(nodeIndex == -1); 
+}
+
+bool Node::isNeighborValid(int nodeID) const {
+    return (neighbors.find(nodeID) != neighbors.end());
+}
+
 bool Node::isInRadiusDomain(Vector2 position) const {
     float dx = position.x - nodePosition.x;
     float dy = position.y - nodePosition.y;
-    return dx * dx + dy * dy <= nodeRadius * nodeRadius; 
+    return (dx * dx + dy * dy <= nodeRadius * nodeRadius); 
+}
+
+void Node::addNeighbor(int nodeID) {
+    neighbors.insert(nodeID);
+}
+
+void Node::changeNodeStatus() {
+    isActive = !isActive;
 }
 
 void Node::drawNode() const {
--- src/node.h
+++ src/node.h
@@ -2,6 +2,7 @@
 #define NODE_H
 
 #include "raylib.h"
+#include <set>
 
 class Node {
 private:
@@ -9,16 +10,23 @@ private:
     Vector2 nodePosition;
     float nodeRadius;
     bool isActive;
+    std::set<int> neighbors;
 
 public:
     Node(int index, Vector2 position, float radius);
+    Node();
     
     int getNodeIndex() const;
     Vector2 getNodePosition() const;
     float getNodeRadius() const;
-    bool isNodeActive() const;
 
+    bool isNodeActive() const;
+    bool isNodeValid() const;
+    bool isNeighborValid(int nodeID) const;
     bool isInRadiusDomain(Vector2 mousePosition) const;
+
+    void addNeighbor(int nodeID);
+    void changeNodeStatus();
     void drawNode() const;
 };
 
