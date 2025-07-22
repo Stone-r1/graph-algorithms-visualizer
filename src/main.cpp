64ed09e2cc69adefe73eaa7743b8159eeca4b082
stoney <sarkisyanmihail11@gmail.com>
2025-07-22 14:21:10 +0400

    feat(main): Temporary, working version. (Let's call it V.1)

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,49 @@
 #include "raylib.h"
 #include "sidebar.h"
 
-const float radius = 50.0f;
+#define SMALL 30.0f
+#define MEDIUM 50.0f
+#define LARGE 70.0f
+
+float radius = MEDIUM;
+
+void handleLeftClick(Board& board, Sidebar& sidebar, Vector2 mouse) {
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) { 
+        if (!sidebar.isInSidebarDomain(mouse, radius)) {
+            board.addNode(mouse, radius);
+        } else {
+            sidebar.handleMouse(mouse);
+
+            if (sidebar.isButtonClicked("Small")) radius = SMALL;
+            if (sidebar.isButtonClicked("Medium")) radius = MEDIUM; 
+            if (sidebar.isButtonClicked("Large")) radius = LARGE;
+            
+
+            sidebar.resetClicks();
+        }
+    }
+}
+
+void handleRightClick(Board& board, Sidebar& sidebar, Vector2 mouse, Vector2& selectedNode) {
+    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+        Vector2 nodePosition = board.isInNodeDomain(mouse);
+        if (nodePosition.x != 0.0f && nodePosition.y != 0.0f) {
+            if (selectedNode.x == -1) {
+                selectedNode.x = nodePosition.x;
+                selectedNode.y = nodePosition.y;
+                std::cout << "Node 1 was chosen successfully\n";
+            } else if (selectedNode.x == nodePosition.x && selectedNode.y == nodePosition.y) {
+                // so basically If you clicked on the same node twice
+                std::cout << "Selected the starting node.\n";
+                selectedNode = {-1, -1};
+            } else {
+                std::cout << "Added the edge between Node 1 and Node2\n";
+                board.addEdge(selectedNode, nodePosition);
+                selectedNode = {-1, -1};
+            }
+        }
+    }
+}
 
 int main() {
     int monitor = GetCurrentMonitor();
@@ -17,37 +59,17 @@ int main() {
 
     while (!WindowShouldClose()) { 
         Vector2 mouse = GetMousePosition();
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            sidebar.handleMouse(mouse);
-            if (!sidebar.isInSidebarDomain(mouse, radius)) {
-                board.addNode(mouse, radius);
-            }
-        }
-
-        if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-            Vector2 nodePosition = board.isInNodeDomain(mouse);
-            if (nodePosition.x != 0.0f && nodePosition.y != 0.0f) {
-                if (selectedNode.x == -1) {
-                    selectedNode.x = nodePosition.x;
-                    selectedNode.y = nodePosition.y;
-                    std::cout << "Node 1 was chosen successfully\n";
-                } else {
-                    std::cout << "Added the edge between Node 1 and Node2\n";
-                    board.addEdge(selectedNode, nodePosition);
-                    selectedNode = {-1, -1};
-                }
-            }
-        }
+        handleLeftClick(board, sidebar, mouse);
+        handleRightClick(board, sidebar, mouse, selectedNode);
 
         BeginDrawing();
             ClearBackground(WHITE);
             sidebar.draw();
-            board.drawNodes();
             board.drawEdges();
+            board.drawNodes();
 
         EndDrawing();
     }
 
     CloseWindow();
 }
-
